cdae503b36a24488dfb6bcee7406c157e41e8ea9
sheikhShahzaib19 <sheikhshahzaib2525@gmail.com>
2022-06-13 12:37:56 +0500

    Updates

---
+++ precache-manifest.6ac5ad04ff5fd0c5bb082ddf199395e1.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "d4f5f33f3bd21e5dbdf5",
+    "url": "/myapp1/static/css/main.4ce70e21.chunk.css"
+  },
+  {
+    "revision": "d4f5f33f3bd21e5dbdf5",
+    "url": "/myapp1/static/js/main.d4f5f33f.chunk.js"
+  },
+  {
+    "revision": "3cdb6d799a8243ae06c8",
+    "url": "/myapp1/static/css/1.ee0a4fae.chunk.css"
+  },
+  {
+    "revision": "3cdb6d799a8243ae06c8",
+    "url": "/myapp1/static/js/1.3cdb6d79.chunk.js"
+  },
+  {
+    "revision": "b83d242e282a96e6ef96",
+    "url": "/myapp1/static/js/runtime~main.b83d242e.js"
+  },
+  {
+    "revision": "fd6ad5fa79d106bdb0ba5cd575b6deaf",
+    "url": "/myapp1/static/media/myimg.fd6ad5fa.jpg"
+  },
+  {
+    "revision": "b44115aeb8207967946c887a8a04103a",
+    "url": "/myapp1/static/media/apple.b44115ae.png"
+  },
+  {
+    "revision": "07468bcdb987f2c19ff69ef79990ffa4",
+    "url": "/myapp1/static/media/port1.07468bcd.png"
+  },
+  {
+    "revision": "009b04cba3b33760ea783f0a092a757b",
+    "url": "/myapp1/static/media/port2.009b04cb.png"
+  },
+  {
+    "revision": "8b21af286b9c692fe5fe878a0cbcfd9b",
+    "url": "/myapp1/static/media/port3.8b21af28.png"
+  },
+  {
+    "revision": "d3fdb50954e9fe89c3c513294278fe91",
+    "url": "/myapp1/static/media/port4.d3fdb509.png"
+  },
+  {
+    "revision": "ea7bcbc21a46845e594eb373e40ee5f8",
+    "url": "/myapp1/index.html"
+  }
+];
